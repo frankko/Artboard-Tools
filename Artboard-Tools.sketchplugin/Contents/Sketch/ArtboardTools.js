2743445152befaab2bfa18e045f384db0a2e023f
frank kolodziej <frank@cacophony.com>
2017-07-13 09:40:48 -0400

    bugfix / rewrite; fixes #7; fixes #13

---
+++ Artboard-Tools.sketchplugin/Contents/Sketch/ArtboardTools.js
@@ -0,0 +1,357 @@
+var KOLOArtboardTools = {
+  "use_slashes": false,
+  "sort_top_to_bottom": false,
+  "arrangeArtboards": function (context) {
+    // User-adjustable:
+
+    var group_related = true;
+    var zoom_to_fit = true;
+
+    var start_x = 16;
+    var start_y = 16;
+
+    var spacing_x = 128;
+    var spacing_y = 256;
+    var spacing_group = Math.round(spacing_x / 2);
+
+    var max_cols = 11;
+
+    // Main junk, don't tread on me
+
+    var doc = context.document;
+    var selection = context.selection;
+    var page = [doc currentPage];
+    var view = [doc currentView];
+
+    var sort_top_to_bottom = KOLOArtboardTools.sort_top_to_bottom;
+
+    var curr_x = start_x;
+    var curr_y = start_y;
+    var curr_cols = 0;
+
+    var max_right = 0;
+    var max_bottom = 0;
+
+    var prev_group_item = null;
+    var curr_row_height = 0;
+
+    var sorted_artboards = KOLOArtboardTools.util.get_all_artboards(context);
+
+    for (var i = 0; i < sorted_artboards.length; i++) {
+      var new_x, new_y;
+      var up_counts = true;
+
+      var ab = sorted_artboards[i];
+      var artboard_name = [ab name];
+
+      var m = artboard_name.match(/^(.+)--([0-9]+)$/i);
+
+      if ((m != null) && (group_related == true)) {
+        var this_group = m[1];
+
+        if (prev_group_item == null) {
+          new_x = curr_x;
+          new_y = curr_y;
+        } else {
+          new_x = [[prev_group_item absoluteRect] x];
+          new_y = [[prev_group_item absoluteRect] y] + [[prev_group_item absoluteRect] height] + spacing_group;
+        }
+
+        prev_group_item = ab;
+
+        if ((i + 1) < sorted_artboards.length) {
+          artboard_next_name = sorted_artboards[i+1].name;
+          var m_next = artboard_next_name.match(/^(.+)--([0-9]+)$/i);
+          if (m_next != null) {
+            if (m_next[1] == this_group) {
+              up_counts = false;
+            } else {
+              prev_group_item = null;
+            }
+          } else {
+            prev_group_item = null;
+          }
+        }
+      } else {
+        prev_group_item = null;
+        new_x = curr_x;
+        new_y = curr_y;
+      }
+
+      [[ab frame] setX:new_x];
+      [[ab frame] setY:new_y];
+
+      var artboard_right = [[ab absoluteRect] x] + [[ab absoluteRect] width];
+      var artboard_bottom = [[ab absoluteRect] y] + [[ab absoluteRect] height];
+
+      max_right = (artboard_right > max_right) ? artboard_right : max_right;
+      max_bottom = (artboard_bottom > max_bottom) ? artboard_bottom : max_bottom;
+      curr_row_height = (artboard_bottom > curr_row_height) ? artboard_bottom : curr_row_height;
+
+      if (up_counts == true) {
+        curr_x += [[ab absoluteRect] width] + spacing_x;
+        curr_cols++;
+
+        if (curr_cols >= max_cols) {
+          curr_x = start_x;
+          curr_y = curr_row_height + spacing_y;
+          curr_cols = 0;
+          curr_row_height = 0;
+        }
+      }
+    }
+
+    if (zoom_to_fit == true) {
+      [view centerLayersInCanvas];
+    }
+
+    [doc showMessage:"Artboards arranged."];
+  },
+  "groupArtboards": function (context) {
+    // User-adjustable:
+
+    var zoom_to_fit = true;
+
+    var start_x = 16;
+    var start_y = 16;
+
+    var spacing_x = 128;
+    var spacing_y = 128;
+    var spacing_group = Math.round(spacing_x / 2);
+
+    // Main junk, don't tread on me
+
+    var sort_top_to_bottom = KOLOArtboardTools.sort_top_to_bottom;
+    var use_slashes = KOLOArtboardTools.use_slashes;
+
+    if (use_slashes) {
+      var regexp_patt = /^(.+)\/.+/i;
+    } else {
+      var regexp_patt = /^(.+)--.+/i;
+    }
+
+    var doc = context.document;
+    var selection = context.selection;
+    var page = [doc currentPage];
+    var view = [doc currentView];
+
+    var curr_x = start_x;
+    var curr_y = start_y;
+
+    var max_right = 0;
+    var max_bottom = 0;
+
+    var prev_group_item = null;
+    var curr_row_height = 0;
+    var found_headers = false;
+
+    var page_name = [page name];
+    if (page_name == "Symbols") {
+      var regexp_patt = /^(.+)\/.+/i;
+    }
+
+    var sorted_artboards = KOLOArtboardTools.util.get_all_artboards(context);
+
+    for (var j = 0; j < sorted_artboards.length; j++) {
+      var ab = sorted_artboards[j];
+      var artboard_name = [ab name];
+      var m = artboard_name.match(regexp_patt);
+      if (m != null) {
+        found_headers = true;
+      }
+    }
+
+    if (found_headers == true) {
+      for (var i = 0; i < sorted_artboards.length; i++) {
+        var new_x, new_y;
+
+        var ab = sorted_artboards[i];
+        var artboard_name = [ab name];
+
+  //      var m = artboard_name.match(/^(.+)--.+/i);
+        var m = artboard_name.match(regexp_patt);
+
+        if (m != null) {
+          var this_group = m[1];
+
+          if (this_group != prev_group_item) {
+            curr_x = start_x;
+            curr_y = curr_row_height + spacing_y;
+            curr_row_height = 0;
+          }
+
+          prev_group_item = m[1];
+        } else {
+          curr_x = start_x;
+          curr_y = curr_row_height + spacing_y;
+          curr_row_height = 0;
+        }
+
+        new_x = curr_x;
+        new_y = curr_y;
+
+        [[ab frame] setX:new_x];
+        [[ab frame] setY:new_y];
+
+        var artboard_right = [[ab absoluteRect] x] + [[ab absoluteRect] width];
+        var artboard_bottom = [[ab absoluteRect] y] + [[ab absoluteRect] height];
+
+        max_right = (artboard_right > max_right) ? artboard_right : max_right;
+        max_bottom = (artboard_bottom > max_bottom) ? artboard_bottom : max_bottom;
+        curr_row_height = (artboard_bottom > curr_row_height) ? artboard_bottom : curr_row_height;
+
+        curr_x += [[ab absoluteRect] width] + spacing_x;
+      }
+
+      if (zoom_to_fit == true) {
+        [view centerLayersInCanvas];
+      }
+      [doc showMessage:"Artboards grouped."];
+    } else {
+      var app = [NSApplication sharedApplication];
+      if (use_slashes) {
+        [app displayDialog:"Canceling. No artboard names contain “/”" withTitle:"Group Artboards"];
+      } else {
+        [app displayDialog:"Canceling. No artboard names contain “--”" withTitle:"Group Artboards"];
+      }
+    }
+  },
+  "fitArtboard": function (context) {
+    var doc = context.document;
+    var selection = context.selection;
+    var page = [doc currentPage];
+    var view = [doc currentView];
+
+    var target_artboard;
+
+    if ([selection count] > 0) {
+      var selected_artboard = [selection objectAtIndex:0];
+
+      while ([selected_artboard parentGroup] != page) {
+        selected_artboard = [selected_artboard parentGroup];
+      }
+
+      [selected_artboard select:true byExpandingSelection:false];
+      doc.actionsController().actionForID("MSZoomToSelectionAction").zoomToSelection(nil);
+  //    doc.actionsController().actionForID("MSZoomToActualSizeAction").actualSize(nil);
+  //    doc.actionsController().actionForID("MSCenterSelectionInVisibleAreaAction").centerSelectionInVisibleArea(nil);
+    }
+  },
+  "selectPreviousArtboard": function (context) {
+    var doc = context.document;
+    var selection = context.selection;
+    var page = [doc currentPage];
+    var view = [doc currentView];
+
+    var current_zoom = [doc zoomValue];
+  //  log(current_zoom);
+
+    var target_artboard;
+    var sorted_artboards = KOLOArtboardTools.util.get_all_artboards(context);
+
+    if ([selection count] > 0) {
+      var selected_artboard = [selection objectAtIndex:0];
+
+      while ([selected_artboard parentGroup] != page) {
+        selected_artboard = [selected_artboard parentGroup];
+      }
+  
+      var selected_artboard_name = [selected_artboard name];
+
+      var selected_index = -1;
+
+      for (var i = 0; i < sorted_artboards.length; i++) {
+        var ab = sorted_artboards[i];
+
+        if ([ab name] == selected_artboard_name) {
+          selected_index = i;
+          break;
+        }
+      }
+      if (selected_index >= 0) {
+        var prev_index = selected_index - 1;
+        if (prev_index < 0) {
+          prev_index = sorted_artboards.length - 1;
+        }
+        target_artboard = sorted_artboards[prev_index];
+      } else {
+        target_artboard = false;
+      }
+    } else {
+      target_artboard = sorted_artboards[sorted_artboards.length - 1];
+    }
+
+    if (target_artboard != false) {
+      [target_artboard select:true byExpandingSelection:false];
+      doc.actionsController().actionForID("MSZoomToSelectionAction").zoomToSelection(nil);
+    }
+  },
+  "selectNextArtboard": function (context) {
+    var doc = context.document;
+    var selection = context.selection;
+    var page = [doc currentPage];
+    var view = [doc currentView];
+
+    var current_zoom = [doc zoomValue];
+  //  log(current_zoom);
+
+    var target_artboard;
+    var sorted_artboards = KOLOArtboardTools.util.get_all_artboards(context);
+
+    if ([selection count] > 0) {
+      var selected_artboard = [selection objectAtIndex:0];
+
+      while ([selected_artboard parentGroup] != page) {
+        selected_artboard = [selected_artboard parentGroup];
+      }
+  
+      var selected_artboard_name = [selected_artboard name];
+      var selected_index = -1;
+
+      for (var i = 0; i < sorted_artboards.length; i++) {
+        var ab = sorted_artboards[i];
+
+        if ([ab name] == selected_artboard_name) {
+          selected_index = i;
+          break;
+        }
+      }
+      if (selected_index >= 0) {
+        var next_index = selected_index + 1;
+        if (next_index >= sorted_artboards.length) {
+          next_index = 0;
+        }
+        target_artboard = sorted_artboards[next_index];
+      } else {
+        target_artboard = false;
+      }
+    } else {
+      target_artboard = sorted_artboards[0];
+    }
+
+    if (target_artboard != false) {
+      [target_artboard select:true byExpandingSelection:false];
+      doc.actionsController().actionForID("MSZoomToSelectionAction").zoomToSelection(nil);
+    }
+  },
+  "util": {
+    "get_all_artboards": function(context) {
+        var all_artboards = context.document.currentPage().artboards();
+        var sorted_artboards = [];
+
+        if (KOLOArtboardTools.sort_top_to_bottom) {
+          var artboards_count = [all_artboards count];
+          for (var i = (artboards_count - 1); i >= 0; i--) {
+            var ab = [all_artboards objectAtIndex:i];
+            sorted_artboards.push(ab);
+          }
+        } else {
+          for (var i = 0; i < [all_artboards count]; i++) {
+            var ab = [all_artboards objectAtIndex:i];
+            sorted_artboards.push(ab);
+          }
+        }
+        return sorted_artboards;
+    }
+  }
+};
--- README.md
+++ README.md
@@ -12,11 +12,6 @@ A A A A
 A A
 ```
 
-If you know what you’re doing
-
-- layer sort order can be changed from bottom-to-top to top-to-bottom by changing `var sort_top_to_bottom = false;` to `var sort_top_to_bottom = true;` in the file `Artboard-Tools.sketchplugin/Contents/Sketch/arrange_artboards.cocoascript`.
-- max number of columns and space between artboards is editable in the file `Artboard-Tools.sketchplugin/Contents/Sketch/arrange_artboards.cocoascript`.]
-
 ## Group Artboards ( ctrl+opt+1 )
 
 Similar to “Arrange Artboards”, but more complicated. Instead of a predefined number of artboards per row, the plugin searches for artboards whose names match a pattern and includes them all on one row, starting a new row when the pattern is broken. Like so:
@@ -31,12 +26,6 @@ Useful if you’re designing a bunch of screens and you wanted each section to b
 
 The pattern searched for goes like this: ‘group name’ + ‘\-\-’ + ‘whatever’. For example, ‘headers\-\-version-A’, ‘headers\-\-version-B’, ‘sidebars\-\-version-A’, ‘footers\-\-version-A’, ‘footers\-\-version-B’. That would create three rows.
 
-If you know what you’re doing:
-
-- the separator can be changed from ‘--’ to ‘/’ by changing `var use_slashes = false;` to `var use_slashes = true;` in the file `Artboard-Tools.sketchplugin/Contents/Sketch/group_artboards.cocoascript`.
-- layer sort order can be changed from bottom-to-top to top-to-bottom by changing `var sort_top_to_bottom = false;` to `var sort_top_to_bottom = true;` in the file `Artboard-Tools.sketchplugin/Contents/Sketch/group_artboards.cocoascript`.
-- space between artboards can be edited in the file `Artboard-Tools.sketchplugin/Contents/Sketch/group_artboards.cocoascript`.
-
 ## Zoom Artboard to Fit ( cmd+4 )
 
 The equivalent of selecting an artboard and doing “Zoom Selection” (cmd+2), but this plugin always zooms out and centers the current artboard, even if you’ve selected a layer within the artboard.
@@ -49,21 +38,21 @@ If no artboard (or a layer within an artboard) is selected, Prev will select the
 
 * * * * *
 
-**Note that for all plugins, the artboard order is determined by the artboard’s position in the layer list _from bottom to top_. Current artboard position on the canvas is ignored.**
+**Note that for all plugins, the artboard order is determined by the artboard’s position in the layer list _from bottom to top_. Current artboard position on the canvas is ignored. See the Roadmap section below on how to change the order to _top to bottom_.**
 
 * * * * *
 
 ### Installation
 
 1. **Easy way:** Install [Sketch Toolbox](http://sketchtoolbox.com/); find “Artboard Tools”; click the “Install” button
-1. **Not-as-easy way:** Download the [plugin zip](https://github.com/frankko/Artboard-Tools/archive/master.zip); open Sketch, go to Plugins → Reveal Plugins Folder…; unzip the downloaded zip into that plugins directory
+1. **Less-easy way:** Download the [plugin zip](https://github.com/frankko/Artboard-Tools/archive/master.zip); unzip the downloaded zip file; double-click the `Artboard-Tools.sketchplugin` file
 
 * * * * *
 
 ### Roadmap
 
-- due to popular demand, the group delimiter will eventually change from ‘--’ to ‘/’. This can be previewed in the Group Artboards plugin by reviewing the instructions above. If you’re opposed to the change, now is your time to make more noise than the folks that want it switched to a slash. In the meantime, if you run Group Artboards on a page called “Symbols”, the plugin will group by ‘/’.
-- added an option to sort layers from top to bottom instead of the default bottom to top. This might become the new default. 
+- due to popular demand, the group delimiter will eventually change from ‘\-\-’ to ‘/’. This can be previewed in the Group Artboards plugin by changing `"use_slashes": false,` to `"use_slashes": true,` at the top of the file `Artboard-Tools.sketchplugin/Contents/Sketch/ArtboardTools.js`. If you’re opposed to the change, now is your time to make more noise than the folks that want it switched to a slash. In the meantime, if you run Group Artboards on a page called “Symbols”, the plugin will always group by ‘/’.
+- added an option to sort layers from top to bottom instead of the default bottom to top. This might become the new default. This can be done by changing `"sort_top_to_bottom": false,` to `"sort_top_to_bottom": true,` at the top of the file `Artboard-Tools.sketchplugin/Contents/Sketch/ArtboardTools.js`. This will apply to all plugins across the board, including Select Next/Prev Artboard.
 
 * * * 
 
